9c78c3465810ca3bb8675372036ef32e1ef444bf
shiva20991 <80451254+shiva20991@users.noreply.github.com>
2022-01-20 09:15:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -639,29 +639,29 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"Here is what i found for your query {search}"
+        cap = f"Here is what i found for your query NOTE  MESSAGE WILL BE AUTOMATICALLY WITH !MIN {search}"
     if imdb and imdb.get('poster'):
         try:
             spa = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(10)
+            await asyncio.sleep(60)
             await spa.delete()
             await message.delete()
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
             spa1 = await message.reply_photo(photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(10)
+            await asyncio.sleep(60)
             await spa1.delete()
             await message.delete()
         except Exception as e:
             logger.exception(e)
             spa2 = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(10)
+            await asyncio.sleep(60)
             await spa2.delete()
             await message.delete()
     else:
         spa3 = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-        await asyncio.sleep(10)
+        await asyncio.sleep(60)
         await spa3.delete()
         await message.delete()
     if spoll:
