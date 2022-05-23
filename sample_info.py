a76c8d81d8e093983d82da2ef500d799f02e217f
Shivayt007 <88087547+Shivayt007@users.noreply.github.com>
2022-05-23 13:02:28 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 6230820
+API_HASH = '4c9af8bc82ea492153ba5eae99b25582'
+BOT_TOKEN = '5351916457:AAFBm67cOdzKVP6d5nMCnuE2n5K9ggyuq8k'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,14 +11,14 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [769411168 ,1845411452,957158815]
+CHANNELS = [-1001594798134]
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'Telegram'
+DATABASE_URI = "mongodb+srv://Tamil_AI_Movies_Bot:Tamil_AI_Movies_Bot@cluster0.uxkht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DATABASE_NAME = 'Cluster0'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 
