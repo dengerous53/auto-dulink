fadbb0bb80bc9b6d54f36abfa3631688b0489438
Shivayt007 <88087547+Shivayt007@users.noreply.github.com>
2022-05-23 15:20:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,9 +32,9 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://duke:duke@cluster0.iebnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappa1n")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files1')
+DATABASE_URI = "mongodb+srv://duke:duke@cluster0.iebnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DATABASE_NAME = "Rajappa1n"
+COLLECTION_NAME = 'Telegram_files1'
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
